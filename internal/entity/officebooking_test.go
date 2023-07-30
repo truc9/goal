d1cf142c2ab2d847203f6316789a200575631256
Truc Nguyen <truc.nguyentrung@hotmail.com>
2023-07-30 08:20:53 +0100

    chore: use test lib

--- internal/entity/officebooking_test.go
+++ internal/entity/officebooking_test.go
@@ -3,18 +3,14 @@ package entity
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateNewPeriod(t *testing.T) {
 	res := CreateNextPeriod()
 	start := res.From.Weekday()
 	end := res.To.Weekday()
-
-	if start != time.Monday {
-		t.Errorf("got %s, wanted Monday", start)
-	}
-
-	if end != time.Sunday {
-		t.Errorf("got %s, wanted Sunday", end)
-	}
+	assert.Equal(t, time.Monday, start, "start of period should be Monday")
+	assert.Equal(t, time.Sunday, end, "end of period should be Sunday")
 }
