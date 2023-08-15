1e180b4219c142b4f4441cfe64814a33234d7aea
Truc Nguyen <truc.nguyentrung@hotmail.com>
2023-08-15 21:06:17 +0100

    fix(test): end date period should be mid-night

--- internal/core/booking_test.go
+++ internal/core/booking_test.go
@@ -1,7 +1,6 @@
 package core
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
@@ -12,10 +11,7 @@ func TestCreateNewPeriod(t *testing.T) {
 	now := time.Date(2023, 8, 4, 0, 0, 0, 0, time.UTC)
 	actual := CreateNextPeriod(now)
 	expectFrom := time.Date(2023, 8, 7, 0, 0, 0, 0, time.UTC)
-	expectTo := time.Date(2023, 8, 13, 0, 0, 0, 0, time.UTC)
-	fmt.Println(actual)
-	fmt.Println(expectFrom)
-	fmt.Println(expectTo)
-	assert.True(t, expectFrom.Equal(actual.From), "start of period should be Monday")
-	assert.True(t, expectTo.Equal(actual.To), "end of period should be Sunday")
+	expectTo := time.Date(2023, 8, 13, 23, 59, 59, 0, time.UTC)
+	assert.True(t, expectFrom.Equal(actual.From))
+	assert.True(t, expectTo.Equal(actual.To))
 }
