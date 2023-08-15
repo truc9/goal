477e9f03599da90aecd43a146ea2948f38bf7837
Truc Nguyen <truc.nguyentrung@hotmail.com>
2023-08-15 21:00:30 +0100

    chore: make icon bigger in booking dashboard

--- web/src/containers/BookingDashboard.tsx
+++ web/src/containers/BookingDashboard.tsx
@@ -40,10 +40,10 @@ const BookingDashboard: React.FC = () => {
             <div className="tw-flex tw-items-center tw-content-center tw-text-center tw-text-2xl tw-justify-center tw-mb-5">
                 <h3>{dayjs(currentPeriod?.from).format('ddd DD MMM YYYY')} - {dayjs(currentPeriod?.to).format('ddd DD MMM YYYY')}</h3>
             </div>
-            <table className='tw-w-full tw-table'>
+            <table className='tw-w-full tw-table tw-border'>
                 <thead>
                     <tr>
-                        <th className="tw-w-[100px]"></th>
+                        <th className="tw-bg-slate-100 tw-w-[200px]"></th>
                         {dates.map((d, i) => {
                             return (
                                 <th key={i} className="tw-w-[130px] tw-h-10 tw-bg-slate-100 tw-justify-center tw-items-center">{dayjs(d).format('ddd')}</th>
@@ -54,9 +54,9 @@ const BookingDashboard: React.FC = () => {
                 <tbody>
                     {userBookings.map((ub: UserBooking, idx: number) => {
                         return (
-                            <tr className="tw-table-row tw-border hover:tw-bg-green-50" key={idx}>
+                            <tr className="tw-table-row hover:tw-bg-green-50" key={idx}>
                                 <td className="tw-bg-slate-100">
-                                    <span className="tw-justify-start tw-pl-5 tw-items-center tw-flex">{ub.userDisplayName}</span>
+                                    <span className="tw-justify-start tw-px-5 tw-items-center tw-flex">{ub.userDisplayName}</span>
                                 </td>
                                 {dates.map((date, i) => {
                                     const booking = ub.bookings.find(bd => dayjs(bd.bookingDate).date() == dayjs(date).date())
@@ -64,11 +64,11 @@ const BookingDashboard: React.FC = () => {
                                         <td key={i} className="tw-p-2">
                                             {booking ? (
                                                 <span className="tw-text-emerald-500 tw-justify-center tw-items-center tw-flex">
-                                                    <IoCheckmarkCircle size="26" />
+                                                    <IoCheckmarkCircle size="36" />
                                                 </span>
                                             ) : (
                                                 <span className="tw-text-orange-200 tw-justify-center tw-items-center tw-flex">
-                                                    <IoCheckmarkCircleOutline size="26" />
+                                                    <IoCheckmarkCircleOutline size="36" />
                                                 </span>
                                             )}
                                         </td>
