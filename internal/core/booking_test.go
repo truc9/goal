2c72de3b307a133b109394df9f122b3f50a1d21b
Truc Nguyen <truc.nguyentrung@hotmail.com>
2023-08-19 08:13:54 +0100

    fix: update test

--- internal/core/booking_test.go
+++ internal/core/booking_test.go
@@ -11,7 +11,7 @@ func TestCreateNewPeriod(t *testing.T) {
 	now := time.Date(2023, 8, 4, 0, 0, 0, 0, time.UTC)
 	actual := CreateNextPeriod(now)
 	expectFrom := time.Date(2023, 8, 7, 0, 0, 0, 0, time.UTC)
-	expectTo := time.Date(2023, 8, 13, 23, 59, 59, 0, time.UTC)
+	expectTo := time.Date(2023, 8, 13, 0, 0, 0, 0, time.UTC)
 	assert.True(t, expectFrom.Equal(actual.From))
 	assert.True(t, expectTo.Equal(actual.To))
 }
