f6d1202f8ff85dc44f81f5e33cf214285c74dcb3
Truc Nguyen <truc.nguyentrung@hotmail.com>
2023-10-14 18:59:10 +0100

    chore: clean up

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -15,9 +15,6 @@ const BookingPeriods = lazy(() => import('./containers/BookingPeriods'))
 const Home = lazy(() => import("./containers/Home"))
 const Login = lazy(() => import('./containers/Login'))
 const Register = lazy(() => import('./containers/Register'))
-const Hse = lazy(() => import('./containers/HSE'))
-const AssessmentVersions = lazy(() => import('./containers/HSE/AssessmentVersions'))
-const AssessmentQuestionSetup = lazy(() => import('./containers/HSE/AssessmentQuestionSetup'))
 const AssessmentSetup = lazy(() => import('./containers/AssessmentSetup'))
 const ASVersions = lazy(() => import("./containers/AssessmentSetup/Versions"))
 const ASQuestions = lazy(() => import("./containers/AssessmentSetup/Questions"))
@@ -34,9 +31,6 @@ function App() {
         <Route path='my-booking' element={<MyBooking />} />
         <Route path='booking-periods' element={<BookingPeriods />} />
         <Route path='booking-dashboard' element={<BookingDashboard />} />
-        <Route path='hse' element={<Hse />} />
-        <Route path='hse/:id' element={<AssessmentVersions />} />
-        <Route path='hse/:id/versions/:versionId' element={<AssessmentQuestionSetup />} />
         <Route path='assessments' element={<AssessmentSetup />}>
           <Route path=':assessmentId' element={<ASVersions />}>
             <Route path='versions' element={<ASQuestions />}>
