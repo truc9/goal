45f319a5b41efce275533140dd23b9bc5bda59d2
Truc Nguyen <truc.nguyentrung@hotmail.com>
2023-10-19 18:15:35 +0100

    chore: spinner total employee stat

--- web/src/components/ClickableCard.tsx
+++ web/src/components/ClickableCard.tsx
@@ -2,41 +2,41 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 interface Props {
-  disabled?: boolean
-  title: string
-  subTitle: string
-  link: string
-  icon: React.ReactNode
+	disabled?: boolean
+	title: string
+	subTitle: string
+	link: string
+	icon: React.ReactNode
 }
 
 export const ClickableCard: React.FC<Props> = ({
-  title,
-  subTitle,
-  link,
-  icon,
-  disabled
+	title,
+	subTitle,
+	link,
+	icon,
+	disabled
 }) => {
-  const content = (
-    <>
-      <span>{icon}</span>
-      <h3 className="tw-text-2xl">{title}</h3>
-      <h5 className="tw-text-xs tw-text-black/40">{subTitle}</h5>
-    </>
-  )
+	const content = (
+		<>
+			<span>{icon}</span>
+			<h3 className='tw-text-2xl'>{title}</h3>
+			<h5 className='tw-text-xs tw-text-black/40'>{subTitle}</h5>
+		</>
+	)
 
-  if (disabled) {
-    return (
-      <div className="tw-flex tw-flex-col tw-items-center tw-gap-3 tw-rounded tw-bg-slate-200 tw-p-5 tw-text-center tw-text-black/40">
-        {content}
-      </div>
-    )
-  }
+	if (disabled) {
+		return (
+			<div className='tw-flex tw-flex-col tw-items-center tw-gap-3 tw-rounded tw-bg-slate-200 tw-p-5 tw-text-center tw-text-black/40'>
+				{content}
+			</div>
+		)
+	}
 
-  return (
-    <Link
-      to={link}
-      className="tw-flex tw-flex-col tw-items-center tw-gap-3 tw-rounded tw-border tw-border-slate-200 tw-bg-slate-50 tw-p-5 tw-text-center hover:tw-bg-slate-100">
-      {content}
-    </Link>
-  )
+	return (
+		<Link
+			to={link}
+			className='tw-flex tw-flex-col tw-items-center tw-gap-3 tw-rounded tw-border tw-border-slate-200 tw-bg-slate-50 tw-p-5 tw-text-center hover:tw-bg-slate-100'>
+			{content}
+		</Link>
+	)
 }
--- web/src/containers/Home/index.tsx
+++ web/src/containers/Home/index.tsx
@@ -1,20 +1,7 @@
-import {
-    useEffect,
-    useState,
-} from 'react'
+import { useEffect, useState } from 'react'
 
-import {
-    FiBarChart2,
-    FiCalendar,
-    FiCheckCircle,
-    FiGrid,
-} from 'react-icons/fi'
-import {
-    Legend,
-    Pie,
-    PieChart,
-    Tooltip,
-} from 'recharts'
+import { FiBarChart2, FiCalendar, FiCheckCircle, FiGrid } from 'react-icons/fi'
+import { Legend, Pie, PieChart, Tooltip } from 'recharts'
 
 import { Card } from '../../components/Card'
 import { ClickableCard } from '../../components/ClickableCard'
@@ -26,68 +13,98 @@ import httpService from '../../services/httpClient'
 import { AsyncContent } from '../../components/AsyncContent'
 
 const Home = () => {
-    const [totalEmployee, setTotalEmployee] = useState(0)
-    const [stats, setStats] = useState<any[]>([])
-    const [data, setData] = useState<{ total: number, booked: number, unbooked: number }>({ total: 0, unbooked: 0, booked: 0 })
-    const { user } = useLocalAuth()
-    const socket = useWebSocket()
-    const [loadingStat, setLoadingStat] = useState(true)
+	const [totalEmployee, setTotalEmployee] = useState(0)
+	const [stats, setStats] = useState<any[]>([])
+	const [data, setData] = useState<{
+		total: number
+		booked: number
+		unbooked: number
+	}>({ total: 0, unbooked: 0, booked: 0 })
+	const { user } = useLocalAuth()
+	const socket = useWebSocket()
+	const [isLoading, setIsLoading] = useState(true)
 
-    socket.handleEvent(NotificationEvents.BookingUpdated, (data) => {
-        console.log(`${new Date()} dashboard updated with data ${JSON.stringify(data)}`)
-        const { total, booked, unbooked } = data.payload.stat
-        setData({ total, booked, unbooked })
-    })
+	socket.handleEvent(NotificationEvents.BookingUpdated, (data) => {
+		setIsLoading(true)
+		console.log(
+			`${new Date()} dashboard updated with data ${JSON.stringify(data)}`
+		)
+		const { total, booked, unbooked } = data.payload.stat
+		setData({ total, booked, unbooked })
+		setIsLoading(false)
+	})
 
-    useEffect(() => {
-        handleLoad()
-    }, [])
+	useEffect(() => {
+		handleLoad()
+	}, [])
 
-    useEffect(() => {
-        setTotalEmployee(data.total)
-        setStats([
-            { name: 'Booked', value: data.booked, fill: "#2dd4bf" },
-            { name: 'Unbooked', value: data.unbooked, fill: "#fb7185" },
-        ])
-    }, [data])
+	useEffect(() => {
+		setTotalEmployee(data.total)
+		setStats([
+			{ name: 'Booked', value: data.booked, fill: '#2dd4bf' },
+			{ name: 'Unbooked', value: data.unbooked, fill: '#fb7185' }
+		])
+	}, [data])
 
-    const handleLoad = async () => {
-        const { booked, unbooked, total } = await httpService.get('stats/booking-overall')
-        setData({ total, booked, unbooked })
-        setLoadingStat(false)
-    }
+	const handleLoad = async () => {
+		const { booked, unbooked, total } = await httpService.get(
+			'stats/booking-overall'
+		)
+		setData({ total, booked, unbooked })
+		setIsLoading(false)
+	}
 
-    return (
-        <PageContainer icon={<FiGrid />} title="Apps">
-            <div className="tw-grid tw-grid-cols-3 tw-gap-5">
-                <ClickableCard title="My Booking" subTitle="Booking Office Visit" icon={<FiCheckCircle size={30} />} link="/my-booking" />
-                <ClickableCard disabled={user.role !== 'admin'} title="Booking Dashboard" subTitle="Manage Employee Bookings" icon={<FiBarChart2 size={30} />} link="/booking-dashboard" />
-                <ClickableCard disabled={user.role !== 'admin'} title="Booking Periods" subTitle="Open Booking Periods" icon={<FiCalendar size={30} />} link="/booking-periods" />
-            </div>
-            <div className="tw-grid tw-grid-cols-3 tw-gap-5 tw-mt-3">
-                <Card title="Total Employee">
-                    <h3 className="tw-text-8xl">{totalEmployee}</h3>
-                </Card>
-                <Card title="Booking Overall Status">
-                    <div>
-                        <AsyncContent loading={loadingStat}>
-                            <PieChart width={300} height={300}>
-                                <Pie
-                                    dataKey="value"
-                                    isAnimationActive={false}
-                                    data={stats}
-                                    outerRadius={90}
-                                    label
-                                />
-                                <Tooltip />
-                                <Legend verticalAlign="top" height={36} />
-                            </PieChart>
-                        </AsyncContent>
-                    </div>
-                </Card>
-            </div>
-        </PageContainer>
-    )
+	return (
+		<PageContainer icon={<FiGrid />} title='Apps'>
+			<div className='tw-grid tw-grid-cols-3 tw-gap-5'>
+				<ClickableCard
+					title='My Booking'
+					subTitle='Booking Office Visit'
+					icon={<FiCheckCircle size={30} />}
+					link='/my-booking'
+				/>
+				<ClickableCard
+					disabled={user.role !== 'admin'}
+					title='Booking Dashboard'
+					subTitle='Manage Employee Bookings'
+					icon={<FiBarChart2 size={30} />}
+					link='/booking-dashboard'
+				/>
+				<ClickableCard
+					disabled={user.role !== 'admin'}
+					title='Booking Periods'
+					subTitle='Open Booking Periods'
+					icon={<FiCalendar size={30} />}
+					link='/booking-periods'
+				/>
+			</div>
+			<div className='tw-mt-3 tw-grid tw-grid-cols-3 tw-gap-5'>
+				<Card title='Total Employee'>
+					{isLoading}
+					<AsyncContent loading={isLoading}>
+						<h3 className='tw-text-8xl'>{totalEmployee}</h3>
+					</AsyncContent>
+				</Card>
+				<Card title='Booking Overall Status'>
+					<div>
+						<AsyncContent loading={isLoading}>
+							<PieChart width={300} height={300}>
+								<Pie
+									dataKey='value'
+									isAnimationActive={false}
+									data={stats}
+									outerRadius={90}
+									label
+								/>
+								<Tooltip />
+								<Legend verticalAlign='top' height={36} />
+							</PieChart>
+						</AsyncContent>
+					</div>
+				</Card>
+			</div>
+		</PageContainer>
+	)
 }
 
 export default Home
