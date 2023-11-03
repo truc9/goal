c43c50e7c4403fd9da75dd4b0864f77cda6563ef
Truc Nguyen <truc.nguyentrung@hotmail.com>
2023-11-03 06:05:43 +0000

    chore: my assessment enhancement

--- web/src/containers/MyAssessment/MyAssessmentDetails.tsx
+++ web/src/containers/MyAssessment/MyAssessmentDetails.tsx
@@ -77,7 +77,51 @@ const MyAssessmentDetails = () => {
 			showGoBack
 			title={questions?.length > 0 ? `Question ${currentIndex}/${questions?.length ?? 0}` : 'No Questions'}>
 			<AsyncContent loading={isLoading}>
-				{questions?.length === 0 && (
+				{questions?.length > 0 && (
+					<div className='flex flex-col'>
+						<div className='flex items-center justify-between'>
+							{currentIndex === 1 ? (
+								<span className='flex w-32 items-center justify-center gap-2 rounded bg-slate-300 p-3 text-center text-xl'>
+									<FiChevronLeft />
+									Prev
+								</span>
+							) : (
+								<button
+									onClick={goPrev}
+									className='flex w-32 items-center justify-center gap-2 rounded bg-rose-500 p-3 text-center text-xl text-white active:-translate-x-1'>
+									<FiChevronLeft />
+									Prev
+								</button>
+							)}
+
+							{question && (
+								<div className='flex items-center justify-center'>
+									<span className='rounded bg-emerald-500 p-1 text-xs text-white'>
+										{QuestionTypeDict[question.type]}
+									</span>
+								</div>
+							)}
+
+							{currentIndex === questions?.length ? (
+								<button
+									onClick={goNext}
+									className='flex items-center justify-center gap-2 rounded bg-emerald-500 p-3 text-center text-xl text-white active:translate-x-1'>
+									<FiSearch />
+									Review
+								</button>
+							) : (
+								<button
+									onClick={goNext}
+									className='flex w-32 items-center justify-center gap-2 rounded bg-orange-500 p-3 text-center text-xl text-white active:translate-x-1'>
+									<FiChevronRight />
+									Next
+								</button>
+							)}
+						</div>
+					</div>
+				)}
+
+				{!isLoading && questions?.length === 0 && (
 					<div className='flex items-center justify-center gap-3 text-center text-xl text-slate-300'>
 						<FiCoffee />
 						<span>Oops! It is an empty assessment. Go and make your coffee</span>
@@ -87,12 +131,6 @@ const MyAssessmentDetails = () => {
 				{question && (
 					<div className='flex flex-col gap-3 text-center'>
 						<div className='flex items-center justify-center'>
-							<span className='rounded bg-emerald-500 p-1 text-xs text-white'>
-								{QuestionTypeDict[question.type]}
-							</span>
-						</div>
-
-						<div className='flex items-center justify-center'>
 							<div className='flex w-1/2 justify-center rounded-lg border border-slate-200 bg-slate-100 p-2 text-center text-xl'>
 								<span>{question.description}</span>
 							</div>
@@ -101,10 +139,12 @@ const MyAssessmentDetails = () => {
 						<div className='flex items-center justify-center'>
 							<div className='flex w-1/2 justify-start rounded-lg border border-slate-200 bg-slate-100 p-5'>
 								{question.type == QuestionType.YesNo && (
-									<div className='flex flex-col gap-3'>
+									<div className='flex flex-col rounded-lg p-2'>
 										{['Yes', 'No'].map((c) => {
 											return (
-												<div key={c.toLowerCase()} className='flex items-center gap-3'>
+												<div
+													key={c.toLowerCase()}
+													className='flex items-center gap-3 rounded-lg p-3 hover:bg-slate-200'>
 													<input
 														className='text-2x h-8 w-8'
 														id={`c${c.toLowerCase()}`}
@@ -113,7 +153,11 @@ const MyAssessmentDetails = () => {
 														value={c}
 														onChange={(e: any) => answersUpdate(e.target.value === 'Yes')}
 													/>
-													<label htmlFor={`c${c.toLocaleLowerCase()}`}>{c}</label>
+													<label
+														htmlFor={`c${c.toLocaleLowerCase()}`}
+														className='hover:cursor-pointer'>
+														{c}
+													</label>
 												</div>
 											)
 										})}
@@ -121,14 +165,16 @@ const MyAssessmentDetails = () => {
 								)}
 
 								{question.type == QuestionType.YesNoNA && (
-									<div className='flex flex-col gap-3'>
+									<div className='flex flex-col'>
 										{[
 											{ name: 'Yes', value: 'yes' },
 											{ name: 'No', value: 'no' },
 											{ name: 'N/A', value: 'na' }
 										].map((c) => {
 											return (
-												<div key={c.value} className='flex items-center gap-3'>
+												<div
+													key={c.value}
+													className='flex items-center gap-3 rounded-lg p-3 hover:bg-slate-200'>
 													<input
 														className='text-2x h-8 w-8'
 														id={`c${c.value}`}
@@ -136,7 +182,9 @@ const MyAssessmentDetails = () => {
 														name={`q${question.id}`}
 														onChange={() => answersUpdate(c.value)}
 													/>
-													<label htmlFor={`c${c.value}`}>{c.name}</label>
+													<label htmlFor={`c${c.value}`} className='hover:cursor-pointer'>
+														{c.name}
+													</label>
 												</div>
 											)
 										})}
@@ -155,10 +203,12 @@ const MyAssessmentDetails = () => {
 								)}
 
 								{question.type === QuestionType.SingleChoice && (
-									<div className='flex flex-col gap-3'>
+									<div className='flex flex-col'>
 										{question.choices?.map((c) => {
 											return (
-												<div key={c.id} className='flex items-center gap-3'>
+												<div
+													key={c.id}
+													className='flex items-center gap-3 rounded-lg p-3 hover:bg-slate-200'>
 													<input
 														className='text-2x h-8 w-8'
 														id={`c${c.id}`}
@@ -167,7 +217,9 @@ const MyAssessmentDetails = () => {
 														name={`q${question.id}`}
 														onChange={(e: any) => answersUpdate(e.target.value)}
 													/>
-													<label htmlFor={`c${c.id}`}>{c.description}</label>
+													<label htmlFor={`c${c.id}`} className='hover:cursor-pointer'>
+														{c.description}
+													</label>
 												</div>
 											)
 										})}
@@ -175,10 +227,12 @@ const MyAssessmentDetails = () => {
 								)}
 
 								{question.type === QuestionType.MultipleChoice && (
-									<div className='flex flex-col gap-3'>
+									<div className='flex flex-col'>
 										{question.choices?.map((c) => {
 											return (
-												<div key={c.id} className='flex items-center gap-3'>
+												<div
+													key={c.id}
+													className='flex items-center gap-3 rounded-lg p-3 hover:bg-slate-200'>
 													<input
 														className='text-2x h-8 w-8'
 														id={`c${c.id}`}
@@ -186,7 +240,9 @@ const MyAssessmentDetails = () => {
 														value={c.id}
 														onChange={answersMultipleChoices}
 													/>
-													<label htmlFor={`c${c.id}`}>{c.description}</label>
+													<label htmlFor={`c${c.id}`} className='hover:cursor-pointer'>
+														{c.description}
+													</label>
 												</div>
 											)
 										})}
@@ -194,44 +250,14 @@ const MyAssessmentDetails = () => {
 								)}
 
 								{question.type === QuestionType.Confirmation && (
-									<div className='flex items-center gap-3'>
+									<div className='flex items-center gap-3 rounded-lg p-3 hover:bg-slate-200'>
 										<input id='confirm' className='text-2x h-8 w-8' type='checkbox' />
-										<label htmlFor='confirm'>Confirm</label>
+										<label htmlFor='confirm' className='hover:cursor-pointer'>
+											Confirm
+										</label>
 									</div>
 								)}
 							</div>
-						</div>
-					</div>
-				)}
-
-				{questions?.length > 0 && (
-					<div className='flex flex-col'>
-						<div className='flex items-center justify-between'>
-							{currentIndex === 1 ? (
-								<span></span>
-							) : (
-								<button
-									onClick={goPrev}
-									className='flex w-32 items-center justify-center gap-2 rounded bg-black p-3 text-center text-xl text-white hover:ring-2 active:-translate-x-1'>
-									<FiChevronLeft />
-									Prev
-								</button>
-							)}
-							{currentIndex === questions?.length ? (
-								<button
-									onClick={goNext}
-									className='flex items-center justify-center gap-2 rounded bg-emerald-500 p-3 text-center text-xl text-white hover:ring-2 active:translate-x-1'>
-									<FiSearch />
-									Review Your Answers
-								</button>
-							) : (
-								<button
-									onClick={goNext}
-									className='flex w-32 items-center justify-center gap-2 rounded bg-black p-3 text-center text-xl text-white hover:ring-2 active:translate-x-1'>
-									<FiChevronRight />
-									Next
-								</button>
-							)}
 						</div>
 					</div>
 				)}
